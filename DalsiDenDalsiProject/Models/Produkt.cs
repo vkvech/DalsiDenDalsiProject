2d558ae8188b59d8649daa134bda5ae4fc1b8c35
vkvech <vasekkvech2003@gmail.com>
2025-05-18 20:14:16 +0200

    Přidejte soubory projektu.

---
+++ DalsiDenDalsiProject/Models/Produkt.cs
@@ -0,0 +1,78 @@
+﻿namespace DalsiDenDalsiProject.Models
+{
+    public class Produkt
+    {
+        public string Nazev { get; set; } = "";
+        private decimal cenaBezDph ;
+        private decimal dph ;
+        public decimal CenaSdpH => CenaBezDph + (CenaBezDph * dph/100);
+        private decimal mnozstvi ;
+        public decimal CenaNaSklade => CenaSdpH * Mnozstvi;
+        public string ChybaDph = "";
+        public string ChybaCenaBezDph = "";
+        public string ChybaMnozstvi = "";
+
+        public decimal CenaBezDph
+        {
+            get
+            {
+                return cenaBezDph;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    cenaBezDph = value;
+                    ChybaCenaBezDph = "";
+                }
+                    
+                else
+                {
+                    cenaBezDph = 0;
+                    ChybaCenaBezDph = "cena musí být větší než 0";
+                }
+                    
+                    
+                
+            }
+        }
+
+        public decimal Dph
+        {
+            get => dph;
+            set
+            {
+                if (value > 0 && value < 100)
+                {
+                    dph = value;
+                    ChybaDph = "";
+                }
+                else
+                {    
+                    dph = 0;
+                    ChybaDph = "rozmezí DPH je 0-100";
+                }
+            }
+        }
+
+        public decimal Mnozstvi
+        {
+            get => mnozstvi;
+            set
+            {
+                if (value > 0)
+                {
+                    mnozstvi = value;
+                    ChybaMnozstvi = "";
+                }
+
+                else
+                {
+                    mnozstvi = 0;
+                    ChybaMnozstvi = "Množství musí být větší než 0";
+                }
+            }
+        }
+    }
+}
